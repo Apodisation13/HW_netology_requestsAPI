03c420cb9a6df8837c9bd4e2263cbde8d767ad50
Pavel Soloshenko <apodisation13@gmail.com>
2021-03-06 17:47:40 +0300

    removed token )))))

--- hw2.py
+++ hw2.py
@@ -12,17 +12,21 @@ class YaUploader:
                                 params={'path': file_path},
                                 headers={'Authorization': f'OAuth {self.token}'})
         # print(response.status_code)
-        href = response.json()['href']
-        # print(href)
-        with open(f'{"D:/" + file_path}', 'rb') as f:
-            r = requests.put(href, files={'file': f})
-        if r.status_code == 201:
-            return print("Успешная загрузка")
-        return print(f"Упс, похоже что-то пошло не так. Ошибка {r.status_code}")
+        if response.status_code == 200:
+            print("Успешно получена ссылка для загрузки файла")
+            href = response.json()['href']
+            # print(href)
+            with open(f'{"D:/" + file_path}', 'rb') as f:
+                r = requests.put(href, files={'file': f})
+            if r.status_code == 201:
+                return print("Успешная загрузка")
+            return print(f"Упс, похоже что-то пошло не так. Ошибка {r.status_code}")
+        else:
+            return print(f'Ошибка получения ссылки для загрузки файла {response.status_code}')
 
 
 if __name__ == '__main__':
-    token = "AgAAAAA854AuAADLWyTnygif80QiiTab_zpBVtM"
+    token = "mytoken"
     file = 'maxresdefault.jpg'  # чисто название файла
     uploader = YaUploader(token)
     uploader.upload(file)
